bb9311253eee32b8a8db2cabcc3b189620216ddd
Erichy-dev <erichnyaga1@gmail.com>
2025-05-19 22:31:19 +0300

    wallet balance

--- lnd-htlc-ui/src/node.rs
+++ lnd-htlc-ui/src/node.rs
@@ -45,7 +45,6 @@ pub async fn node_status(network: &str, window_weak: &Arc<slint::Weak<MainWindow
                 let stdout = String::from_utf8_lossy(&output.stdout);
                 // println!("lncli --network testnet getinfo result:\n{}", stdout);
 
-                // Set node as running since command was successful
                 node_running = true;
 
                 // Simple parsing of relevant fields
@@ -70,11 +69,57 @@ pub async fn node_status(network: &str, window_weak: &Arc<slint::Weak<MainWindow
                 if let Some(i) = stdout.find("\"identity_pubkey\":") {
                     identity_pubkey = stdout[i + 21..i + 87].to_string();
                 }
+                let node_window_weak = window_weak_clone.clone();
                 let _ = slint::invoke_from_event_loop(move || {
-                    if let Some(window) = window_weak_clone.upgrade() {
+                    if let Some(window) = node_window_weak.upgrade() {
                         window.set_wallet_needs_unlock(false);
                     }
                 });
+
+                let wallet_output = if network == "testnet" {
+                    Command::new("/usr/local/bin/lncli")
+                        .args(["--network", "testnet", "walletbalance"])
+                        .output()
+                } else {
+                    Command::new("/usr/local/bin/lncli")
+                        .args(["walletbalance"])
+                        .output()
+                };
+
+                let mut wallet_balance: i32 = 0;
+                match wallet_output {
+                    Ok(wallet_output) => {
+                        if wallet_output.status.success() {
+                            let stdout = String::from_utf8_lossy(&wallet_output.stdout);
+                            const CONFIRMED_BALANCE_KEY: &str = "\"confirmed_balance\":  \"";
+                            if let Some(key_start_index) = stdout.find(CONFIRMED_BALANCE_KEY) {
+                                let value_start_index = key_start_index + CONFIRMED_BALANCE_KEY.len();
+
+                                let possible_balance = stdout[value_start_index..].trim();
+                                let index_of_quote = possible_balance.find("\"");
+                                let balance_str = &possible_balance[..index_of_quote.unwrap()].trim();
+
+                                if let Ok(balance) = balance_str.parse::<i32>() {
+                                    wallet_balance = balance;
+                                } else {
+                                    println!("Failed to parse confirmed balance from string: '{}'", balance_str);
+                                }
+                            } else {
+                                println!("'confirmed_balance' key not found in walletbalance output.");
+                            }
+                        }
+                    }
+                    Err(e) => {
+                        println!("Failed to get wallet balance: {}", e);
+                    }
+                }
+                let wallet_window_weak = window_weak_clone.clone();
+                let _ = slint::invoke_from_event_loop(move || {
+                    if let Some(window) = wallet_window_weak.upgrade() {
+                        window.set_wallet_balance(wallet_balance);
+                    }
+                });
+                
             } else {
                 let stderr = String::from_utf8_lossy(&output.stderr);
                 println!("lncli command failed: {}", stderr);
